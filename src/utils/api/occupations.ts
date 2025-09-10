3d36406f0e3294b6a986912b327814341e64933c
JMichu <michal0048@interia.pl>
2025-09-10 12:50:15 +0200

    fix(api): add API-KEY to header

--- src/utils/api/occupations.ts
+++ src/utils/api/occupations.ts
@@ -5,7 +5,13 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 // GET /api/occupations
 const getAllOccupations = async (): Promise<Occupation[]> => {
-    const res = await fetch(`${API_URL}/api/occupations`);
+    const res = await fetch(`${API_URL}/api/occupations`, {
+        headers: {
+            'API-KEY': import.meta.env.API_KEY
+        }
+    });
     if (!res.ok) throw new Error('Failed to fetch occupations');
     return res.json();
 }
+
+export default getAllOccupations;
