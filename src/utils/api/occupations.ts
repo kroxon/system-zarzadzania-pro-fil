ca5673f770b1ebe433aaf88053d862e8268c3c30
JMichu <michal0048@interia.pl>
2025-09-12 11:26:33 +0200

    feat(api): implement full Occupation API

--- src/utils/api/occupations.ts
+++ src/utils/api/occupations.ts
@@ -1,4 +1,4 @@
-import { Occupation } from "../../types";
+import { Occupation, CreateOccupation, UpdateOccupation } from "../../types";
 
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -14,4 +14,71 @@ const getAllOccupations = async (): Promise<Occupation[]> => {
     return res.json();
 }
 
-export default getAllOccupations;
+// POST /api/occupations
+
+const addNewOccupation = async (token : string, data: CreateOccupation): Promise<Occupation>=> {
+    const res = await fetch(`${API_URL}/api/occupations`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    if(!res.ok)
+    {
+        throw new Error("Failed to add new occupation")
+    }
+    return await res.json();
+}
+
+// GET /api/ocupations/{id}
+
+const getOccupation = async (token : string, id: number): Promise<Occupation>=> {
+    const res = await fetch(`${API_URL}/api/occupations/${id}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    })
+    if(!res.ok)
+    {
+        throw new Error("Failed to fetch occupation")
+    }
+    return await res.json();
+}
+
+// DELETE /api/occupations/{id}
+
+const deleteOccupation = async (token : string, id: number): Promise<Occupation>=> {
+    const res = await fetch(`${API_URL}/api/occupations/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+    })
+    if(!res.ok)
+    {
+        throw new Error("Failed to delete occupation")
+    }
+    return await res.json();
+}
+
+// PUT /api/occupations/{id}
+const updateOccupation = async (token: string, id: number, data: UpdateOccupation): Promise<void> => {
+    const res = await fetch(`${API_URL}/api/occupations/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    if (!res.ok) {
+        throw new Error('Failed to update occupation');
+    }
+};
+
+export { getAllOccupations, addNewOccupation, getOccupation, deleteOccupation, updateOccupation };
